999ea44e947f8b2348329a1dfa28ad63a13c8473
ygarg704 <ygarg704@gmail.com>
2020-10-07 23:09:03 +0530

    html

--- app.py
+++ app.py
@@ -1,46 +1,42 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import re
+import os
 import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
-def button():
+
+@app.route('/second', methods=['POST'])
+def second():
     if request.method == 'POST':
-        comment = request.json['data']
-        comment = comment.split(",")
-        comment = [int(i) for i in comment]
-        print(comment)
+        comment1 = request.form['blue']
+        comment2 = request.form['green']
+        comment3 = request.form['red']
+
+        comment = [comment1, comment2, comment3]
 
         ds = pd.read_csv('dataset/ph-data.csv')
 
         X = ds.iloc[:, :-1]
         X.loc[len(X)] = comment
         X = X.values
-        print(X[-1])
 
         sc = StandardScaler()
         X = sc.fit_transform(X)
 
         model = joblib.load('notebook/model.pkl')
-        pred = model.predict(X)[-1].tolist()
-        print(X[-1])
+        pred = model.predict(X)[-1]
 
-
-        preds = {
-            "Prediction": pred
-        }
-
-        print(preds)
-        return preds
+        print(pred)
+        return render_template("result.html", prediction=pred, comments1=comment1, comments2=comment2, comments3=comment3)
 
 
 if __name__ == '__main__':
     app.run()
-
-
